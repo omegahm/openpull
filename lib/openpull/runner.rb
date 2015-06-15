f94d28f73e75ef90af59e8de549d7fba98d34128
Mads Ohm Larsen <mads.ohm@gmail.com>
2015-06-15 13:44:32 +0200

    Clean up options parser

--- lib/openpull/runner.rb
+++ lib/openpull/runner.rb
@@ -19,64 +19,90 @@ module OpenPull
     def run
       op = option_parser
       op.parse!
-      exit unless check_options(op)
+      check_options(op)
 
       puts OpenPull::Client.new(@options[:access_token],
                                 @options[:organisation],
                                 @options[:username]).show_table
-
     end
 
     private
 
     def option_parser
       OptionParser.new do |opts|
-        opts.banner = 'Usage: openpull [options]'
+        parse_banner(opts)
+        parse_options(opts)
+        parse_values(opts)
+      end
+    end
 
-        opts.separator ''
-        opts.separator 'Organisation and username can also be set in the ' \
-                       'environment as GITHUB_ORGANISATION and GITHUB_USERNAME.'
-        opts.separator ''
+    def parse_banner(opts)
+      opts.banner = 'Usage: openpull [options]'
 
-        opts.separator 'Options:'
+      opts.separator ''
+      opts.separator 'Organisation and username can also be set in the ' \
+                     'environment as GITHUB_ORGANISATION and GITHUB_USERNAME.'
+      opts.separator ''
+    end
 
-        opts.on_tail('-h', '--help', 'Show this message') do
-          puts opts
-          exit
-        end
+    def parse_options(opts)
+      opts.separator 'Options:'
 
-        opts.on_tail('--version', 'Show version') do
-          puts OpenPull::VERSION
-          exit
-        end
+      opts.on_tail('-h', '--help', 'Show this message') do
+        puts opts
+        exit
+      end
 
-        @options[:organisation] = ENV['GITHUB_ORGANISATION']
-        opts.on('-o', '--organisation [ORG]', String, 'The Github organisation') do |o|
-          @options[:organisation] = o
-        end
+      opts.on_tail('--version', 'Show version') do
+        puts OpenPull::VERSION
+        exit
+      end
+    end
 
-        @options[:username] = ENV['GITHUB_USERNAME']
-        opts.on('-u', '--username [USER]', String, 'Your Github username') do |u|
-          @options[:username] = u
-        end
+    def parse_values(opts)
+      parse_organisation(opts)
+      parse_username(opts)
+    end
+
+    def parse_organisation(opts)
+      @options[:organisation] = ENV['GITHUB_ORGANISATION']
+      opts.on('-o',
+              '--organisation [ORG]',
+              String,
+              'The Github organisation') do |o|
+        @options[:organisation] = o
+      end
+    end
+
+    def parse_username(opts)
+      @options[:username] = ENV['GITHUB_USERNAME']
+      opts.on('-u',
+              '--username [USER]',
+              String,
+              'Your Github username') do |u|
+        @options[:username] = u
       end
     end
 
     def check_options(op)
+      check_access_token
+      check_organisation(op)
+    end
+
+    def check_access_token
       @options[:access_token] = ENV['GITHUB_ACCESS_TOKEN']
+      return unless @options[:access_token].nil?
+      puts 'You need to set the GitHub access token in your environment. ' \
+           '(GITHUB_ACCESS_TOKEN)'
+      exit
+    end
 
-      if @options[:access_token].nil?
-        puts 'You need to set the GitHub access token in your environment. ' \
-             '(GITHUB_ACCESS_TOKEN)'
-        return false
-      elsif @options[:organisation].nil?
-        puts 'You need to either pass GitHub organisation or set it in your ' \
-             'environment. (GITHUB_ORGANISATION)'
-        puts "\n#{op}"
-        return false
-      end
-
-      return true
+    def check_organisation(op)
+      return unless @options[:organisation].nil?
+      puts 'You need to either pass GitHub organisation or set it in your ' \
+           'environment. (GITHUB_ORGANISATION)'
+      puts "\n#{op}"
+      exit
     end
   end
 end
